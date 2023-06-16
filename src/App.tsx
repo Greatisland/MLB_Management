207014e3777acd40e83548c9ce010c16d5b0a8d6
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-16 07:15:31 +0000

    Test: notion api 호출 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Client } from "@notionhq/client"
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default class NotionApi {
+//   static async getItem(): Promise<any> {
+//     const notionKey = process.env.REACT_APP_NOTION_KEY
+//     const notionDatabaseKey = process.env.REACT_APP_NOTION_DATABASE_KEY
 
+//     const notion = new Client({ auth: notionKey })
+//     const response = await notion.databases.query({ database_id: notionDatabaseKey })
+//     console.log(response)
+//   }
+// }
+const App = () => {
+  const EndPoint = 'https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/databases/'
+  const notion = new Client({ auth: process.env.NOTION_API_KEY })
+  const notionKey = process.env.NOTION_API_KEY
+  const databaseId = process.env.NOTION_DATABASE_KEY
+
+  const [pages, setPages] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(EndPoint+databaseId+'/query', {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${notionKey}`,
+            'Notion-Version': '2022-06-28',
+            "Content-Type": 'application/json'
+          }
+        });
+        const data = await response.json();
+        setPages(data.results);
+        console.log(data)
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
