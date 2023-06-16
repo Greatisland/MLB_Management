fc91c51be166b2f236a088139063525705750aa6
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-16 07:57:10 +0000

    Test: notion api CRUD test

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,67 @@
-import { Client } from "@notionhq/client"
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
-// export default class NotionApi {
-//   static async getItem(): Promise<any> {
-//     const notionKey = process.env.REACT_APP_NOTION_KEY
-//     const notionDatabaseKey = process.env.REACT_APP_NOTION_DATABASE_KEY
-
-//     const notion = new Client({ auth: notionKey })
-//     const response = await notion.databases.query({ database_id: notionDatabaseKey })
-//     console.log(response)
-//   }
-// }
 const App = () => {
-  const EndPoint = 'https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/databases/'
-  const notion = new Client({ auth: process.env.NOTION_API_KEY })
+  const Cors = 'https://cors-anywhere.herokuapp.com/'
+  const EndPointCreate = 'https://api.notion.com/v1/pages/'
+  const EndPointRead = 'https://api.notion.com/v1/databases/'
+  const EndPointUpdate = 'https://api.notion.com/v1/databases/'
+  const EndPointDelete = 'https://api.notion.com/v1/databases/'
+
   const notionKey = process.env.NOTION_API_KEY
   const databaseId = process.env.NOTION_DATABASE_KEY
 
-  const [pages, setPages] = useState<any[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(EndPoint+databaseId+'/query', {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${notionKey}`,
-            'Notion-Version': '2022-06-28',
-            "Content-Type": 'application/json'
-          }
-        });
-        const data = await response.json();
-        setPages(data.results);
-        console.log(data)
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
+  const readData = async () => {
+    try {
+      const response = await fetch(`${Cors}${EndPointRead}${databaseId}/query`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${notionKey}`,
+          'Notion-Version': '2022-06-28',
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-    fetchData();
-  }, []);
+  const postData = async () => {
+    try {
+      const response = await fetch(`${Cors}${EndPointCreate}`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${notionKey}`,
+          'Notion-Version': '2022-06-28',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          parent: { database_id: databaseId },
+          properties: {
+            Name: {
+              "title": [
+                {
+                  "text": {
+                    "content": "Tuscan Kale"
+                  }
+                }
+              ]
+            }
+          }
+        })
+      })
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error', error)
+    }
+  }
+
   return (
     <>
-
+      <div onClick={() => {readData()}}>읽기</div>
+      <div onClick={() => {postData()}}>쓰기</div>
     </>
   )
 }
