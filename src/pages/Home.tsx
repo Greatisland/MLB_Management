99379f2a55df97974a5076aa0c4644821582b44f
bahn <dontsendloveletters@gmail.com>
2023-06-26 01:39:56 +0900

    Fix: firebase 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,8 @@
 import BtnList from "../components/BtnList"
 import HomeList from "../components/HomeList"
 import { HomeContainer } from "../style/homeStyled"
-import { dbFunc } from "../firebase/firebaseFunc"
-import { useEffect } from "react"
+
 const Home = () => {
-  const member = {
-    name: '김시득',
-    year: '1999',
-    gender: '남'
-  }
-  useEffect(() => {
-    dbFunc.addMember(member)
-  }, [])
   return (
     <HomeContainer>
       <BtnList />
