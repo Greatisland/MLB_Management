91e43b94e4afec76ad87ada5570da2c57ef14530
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-26 08:05:08 +0000

    Fix: Title Link 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import { HeaderContainer } from "../style/headerStyle"
 import MemberModal from "./MemberModal";
 import { useAppSelector } from "../store/hook";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { modalState } = useAppSelector(state => state.membersData)
 
   return (
     <HeaderContainer>
-      <h1>MLB MANAGEMENT</h1>
+      <Link to='/'><h1>MLB MANAGEMENT</h1></Link>
       {modalState ? <MemberModal /> : null}
     </HeaderContainer>
   )
