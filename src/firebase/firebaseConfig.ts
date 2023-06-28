de691d9050157d4b0c1af5f279c6546aea5c062c
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 03:13:54 +0000

    Fix: vite 배포 및 환경변수 재설정

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,23 +1,12 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyAR9z1gLglR7AQ7XRbaWll9mkOmjeRCTio',
-  databaseURL: 'https://mlb-management-default-rtdb.firebaseio.com',
-
-  authDomain: 'mlb-management.firebaseapp.com',
-  projectId: 'mlb-management',
-  storageBucket: 'mlb-management.appspot.com',
-  messagingSenderId: '1029800054629',
-  appId: '102981:1029800054629:web:52de3e19a1267d5a1f1b8c00054629',
-  measurementId: 'G-547PMYFKTY',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
 }
-// const firebaseConfig = {
-//   apiKey: import.meta.env.API_KEY,
-//   authDomain: import.meta.env.AUTHDOMAIN,
-//   projectId: import.meta.env.PROJECTID,
-//   storageBucket: import.meta.env.STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-//   appId: import.meta.env.APPID,
-//   measurementId: import.meta.env.MEASUREMENTID,
-//   databaseURL: import.meta.env.DATABASEURL,
-// }
 
 export default firebaseConfig
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-// import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
-// dotenv.config({ path: '.env.production' }) //배포 환경에서 사용
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/MLB_Management/", 
   plugins: [react()],
 
 })
