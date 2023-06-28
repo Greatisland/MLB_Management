105162189a58259e4a311dde2881d2fb9f8f825c
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 02:16:05 +0000

    Fix: 환경변수 문제

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
-dotenv.config({ path: '.env.production' }) //배포 환경에서 사용
+// dotenv.config({ path: '.env.production' }) //배포 환경에서 사용
 
 // https://vitejs.dev/config/
 export default defineConfig({
