7e89eb20e35d406ba376a25b36aea4df5af2e43f
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 02:18:20 +0000

    Test: 환경변수 제거

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,12 +1,22 @@
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTHDOMAIN,
-  projectId: import.meta.env.PROJECTID,
-  storageBucket: import.meta.env.STORAGEBUCKET,
-  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-  appId: import.meta.env.APPID,
-  measurementId: import.meta.env.MEASUREMENTID,
-  databaseURL: import.meta.env.DATABASEURL,
+  apiKey: 'AIzaSyAR9z1gLglR7AQ7XRbaWll9mkOmjeRCTio',
+  authDomain: 'https://mlb-management-default-rtdb.firebaseio.com',
+  projectId: 'mlb-management.firebaseapp.com',
+  storageBucket: 'mlb-management',
+  messagingSenderId: 'mlb-management.appspot.com',
+  appId: '1029800054629',
+  measurementId: '1:1029800054629:web:52de3e19a1267d5a1f1b8c',
+  databaseURL: 'G-547PMYFKTY'
 }
+// const firebaseConfig = {
+//   apiKey: import.meta.env.API_KEY,
+//   authDomain: import.meta.env.AUTHDOMAIN,
+//   projectId: import.meta.env.PROJECTID,
+//   storageBucket: import.meta.env.STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.MESSAGINGSENDERID,
+//   appId: import.meta.env.APPID,
+//   measurementId: import.meta.env.MEASUREMENTID,
+//   databaseURL: import.meta.env.DATABASEURL,
+// }
 
 export default firebaseConfig
