90210ef1af1676896ad98bef2c4ed93d04a96605
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 02:23:24 +0000

    Test: 환경변수 제거

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,12 +1,13 @@
 const firebaseConfig = {
   apiKey: 'AIzaSyAR9z1gLglR7AQ7XRbaWll9mkOmjeRCTio',
-  authDomain: 'https://mlb-management-default-rtdb.firebaseio.com',
-  projectId: 'mlb-management.firebaseapp.com',
-  storageBucket: 'mlb-management',
-  messagingSenderId: 'mlb-management.appspot.com',
-  appId: '1029800054629',
-  measurementId: '1:1029800054629:web:52de3e19a1267d5a1f1b8c',
-  databaseURL: 'G-547PMYFKTY'
+  databaseURL: 'https://mlb-management-default-rtdb.firebaseio.com',
+
+  authDomain: 'mlb-management.firebaseapp.com',
+  projectId: 'mlb-management',
+  storageBucket: 'mlb-management.appspot.com',
+  messagingSenderId: '1029800054629',
+  appId: '102981:1029800054629:web:52de3e19a1267d5a1f1b8c00054629',
+  measurementId: 'G-547PMYFKTY',
 }
 // const firebaseConfig = {
 //   apiKey: import.meta.env.API_KEY,
