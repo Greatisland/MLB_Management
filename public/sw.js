984e41d1b31be4e19a865ce513dd0b94012a44eb
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 04:40:14 +0000

    Feat: PWA 설정

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/app.js", "/styles.css"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["*.js"], // Or be more specific: ['sw.js']
   plugins: [react()],
 })
