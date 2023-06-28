aed4fcc640ad775112efbbb1152f0e64da2fc635
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-06-28 06:18:25 +0000

    Feat: 서비스워커 등록

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/configureStore.ts'
 
-//서비스 워커 등록
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-        console.log("Service Worker registered with scope: ", registration.scope);
-      },
-      (err) => {
-        console.log("Service Worker registration failed: ", err);
-      }
-    );
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     {/* <BrowserRouter basename='/MLB_Management/'> */}
