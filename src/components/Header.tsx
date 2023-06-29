5b3590fceccbb735afa3012a0888633865c2b257
Greatisland <dontsendloveletters@gmail.com>
2023-06-29 13:16:55 +0900

    Test: test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,6 @@ const Header = () => {
   return (
     <HeaderContainer>
       <Link to='/'><h1 className="eng">MLB MANAGEMENT</h1></Link>
-      <div className="btns">
-      <div onClick={() => authFunc.stateAccount()}>확인</div>
-      <div onClick={() => console.log('xxx')}>먼데</div>
-      <div onClick={() => {authFunc.logout(), console.log('sex')}}>로그아웃</div>
-      {/* <div onClick={() => authFunc.loginGoogle()}>구글 로그인</div> */}
-      {/* <div onClick={() => authFunc.joinFacebook()}>페북 로그인</div> */}
-      </div>
       {modalState ? <MemberModal /> : null}
     </HeaderContainer>
   )
