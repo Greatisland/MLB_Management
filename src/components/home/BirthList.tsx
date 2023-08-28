52fed701e25e324d31b55b9552525d6a336410a4
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-08-28 05:47:46 +0000

    Fix: 다음달 생일자 없을 경우 그 다음달 생일자 보이게 변경 / 12월까지 반복

--- src/components/home/BirthList.tsx
+++ src/components/home/BirthList.tsx
@@ -7,15 +7,24 @@ const BirthList = () => {
   const { membersData } = useAppSelector(state => state.membersData)
   
   //다음달 생일자 명단
-  const birthMemberData = membersData.filter((member) => {
-    const birthMonth = member[1].birth ? member[1].birth.split('월')[0] : null
-    if(Number(birthMonth) === Number(dateCalc('flatMonth')) + 1) return true
-  })
+  const handleBirthMember = (param : number) => {
+    return membersData.filter((member) => {
+      const birthMonth = member[1].birth ? member[1].birth.split('월')[0] : null
+      if(Number(birthMonth) === param) return true
+    })
+  }
+  let monthCount = Number(dateCalc('flatMonth')) + 1
+  let birthMemberData = handleBirthMember(monthCount)
+
+  while(birthMemberData.length === 0 && monthCount <= 12){
+    monthCount++
+    birthMemberData = handleBirthMember(monthCount)
+  }
 
   if(birthMemberData.length > 0){return (
     <PartAwardContainer>
       <FaBirthdayCake />
-      <p>다가오는 <span className="date">{((Number(dateCalc('month')) + 1)).toString().padStart(2,'0')}</span>월 생일인 회원은 <br /><span className="name">{
+      <p>다가오는 <span className="date">{monthCount.toString().padStart(2,'0')}</span>월 생일인 회원은 <br /><span className="name">{
       birthMemberData.map((member, i) => (
         <span className='name' key={i}>{i !== 0 ? ',': null} {member[1]?.name}</span>
       ))
