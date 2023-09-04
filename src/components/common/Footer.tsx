cd07c0eb474839e269caae709499610e6f994aa3
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-09-04 01:57:14 +0000

    Fix: footer 조건문 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -9,12 +9,12 @@ import { useLocation } from 'react-router-dom';
 const Footer = () => {
   const { loginUser } = useAppSelector(state => state.membersData)
   const location = useLocation()
-  const currentPath = location.pathname || '/infopage'
+  const currentPath = location.pathname
   return (
     <>
     {loginUser.state ? 
       <FooterContainer>
-        <Link to='/infopage' className={currentPath === '/infopage' ? 'spot' : 'none'}><AiOutlineUser /><p>회원관리</p></Link>
+        <Link to='/infopage' className={currentPath === '/' || currentPath === '/infopage' ? 'spot' : 'none'}><AiOutlineUser /><p>회원관리</p></Link>
         <Link to='/partpage' className={currentPath === '/partpage' ? 'spot' : 'none'}><AiFillEdit /><p>참석관리</p></Link>
         {loginUser.level >= 2 ? <Link to='/memberfee' className={currentPath === '/memberfee' ? 'spot' : 'none'}><GiMoneyStack /><p>회비관리</p></Link> : null}
         <Link to='/secretboard' className={currentPath === '/secretboard' ? 'spot' : 'none'}><AiFillDatabase /><p>칭찬게시판</p></Link>
