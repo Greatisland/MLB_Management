e85f4967fc8b3ea6d3935a8fa7e5fd520e803c6c
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-09-04 01:43:24 +0000

    Fix: Footer spot 기본값 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -9,15 +9,16 @@ import { useLocation } from 'react-router-dom';
 const Footer = () => {
   const { loginUser } = useAppSelector(state => state.membersData)
   const location = useLocation()
+  const currentPath = location.pathname || '/infopage'
   return (
     <>
     {loginUser.state ? 
       <FooterContainer>
-        <Link to='/infopage' className={location.pathname === '/infopage' ? 'spot' : 'none'}><AiOutlineUser /><p>회원관리</p></Link>
-        <Link to='/partpage' className={location.pathname === '/partpage' ? 'spot' : 'none'}><AiFillEdit /><p>참석관리</p></Link>
-        {loginUser.level >= 2 ? <Link to='/memberfee' className={location.pathname === '/memberfee' ? 'spot' : 'none'}><GiMoneyStack /><p>회비관리</p></Link> : null}
-        <Link to='/secretboard' className={location.pathname === '/secretboard' ? 'spot' : 'none'}><AiFillDatabase /><p>칭찬게시판</p></Link>
-        <Link to='/halloffame' className={location.pathname === '/halloffame' ? 'spot' : 'none'}><AiFillTrophy /><p>명예의 전당</p></Link>
+        <Link to='/infopage' className={currentPath === '/infopage' ? 'spot' : 'none'}><AiOutlineUser /><p>회원관리</p></Link>
+        <Link to='/partpage' className={currentPath === '/partpage' ? 'spot' : 'none'}><AiFillEdit /><p>참석관리</p></Link>
+        {loginUser.level >= 2 ? <Link to='/memberfee' className={currentPath === '/memberfee' ? 'spot' : 'none'}><GiMoneyStack /><p>회비관리</p></Link> : null}
+        <Link to='/secretboard' className={currentPath === '/secretboard' ? 'spot' : 'none'}><AiFillDatabase /><p>칭찬게시판</p></Link>
+        <Link to='/halloffame' className={currentPath === '/halloffame' ? 'spot' : 'none'}><AiFillTrophy /><p>명예의 전당</p></Link>
       </FooterContainer> :
       <></>
     }
