2534936db3f1369594f03e88ddf48e57f51cf8a5
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-09-07 05:42:58 +0000

    Fix: 타입에러 수정

--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -1,9 +1,6 @@
 import Footer from "../components/common/Footer"
 import { GraphPageContainer } from "../style/graphPageStyled"
 import { useAppSelector } from "../store/hook"
-import GraphAttend from "../components/graphPage/GraphAttend"
-import GraphPersonal from "../components/graphPage/GraphPersonal"
-import GraphMeeting from "../components/graphPage/GraphMeeting"
 import HostRanking from "../components/graphPage/HostRanking"
 
 const GraphPage = () => {
