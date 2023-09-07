33d4b06c3878881ed733ba970bb695da849d4c2b
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-09-07 05:55:26 +0000

    Feat: 지난달 랭킹 추가

--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -2,12 +2,14 @@ import Footer from "../components/common/Footer"
 import { GraphPageContainer } from "../style/graphPageStyled"
 import { useAppSelector } from "../store/hook"
 import HostRanking from "../components/graphPage/HostRanking"
+import LatestHostRanking from "../components/graphPage/LatestHostRanking"
 
 const GraphPage = () => {
   const { membersData } = useAppSelector(state => state.membersData)
 
   return (
     <GraphPageContainer>
+      <LatestHostRanking members={membersData} />
       <HostRanking members={membersData} />
       <Footer />
     </GraphPageContainer>
