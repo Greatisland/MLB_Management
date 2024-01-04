f7c6f4bee2a351ddfe9314d37dfc9c4bd8dd3552
THISISHYEONJIN <dontsendloveletters@gmail.com>
2024-01-04 01:02:30 +0000

    feat: 전체 회원 참석율 통계 수정

--- src/components/graphPage/BasicStats.tsx
+++ src/components/graphPage/BasicStats.tsx
@@ -2,15 +2,11 @@ import { BasicStatsContainer } from "../../style/graphPageStyled.tsx"
 import { totalCalcFunc } from "../../lib/totalCalcFunc.ts"
 import { daysBetweenDates } from "../../lib/daysBetweenDates.ts"
 import { useAppSelector } from "../../store/hook.ts"
+import { totalyAttend } from "../../lib/totalyAttend.ts"
 
 const BasicStats = () => {
   const { membersData } = useAppSelector(state => state.membersData)
   const totalMember = membersData.filter(member => !member[1].break && member[1].join)
-
-  //각 멤버 평균참석률의 평균값(해당 년도만)
-  const allMemberAverAttend = Math.round(totalMember.reduce((acc, val) => {
-    return acc + totalCalcFunc(val[1], new Date().getFullYear()).aver
-  }, 0)/totalMember.length * 100)/100
 
   //평균 나이
   const allMemberAverOld = Math.round(totalMember.reduce((acc, val) => {
@@ -29,7 +25,7 @@ const BasicStats = () => {
       <p className="title">전체 회원 통계</p>
       <div className='content'>
         <span className='content_title'>참석율</span>
-        <span className='content_body'><span className="add_text">평균 월</span>{allMemberAverAttend}<span className="add_text">회</span></span>
+        <span className='content_body'><span className="add_text">평균 월</span>{totalyAttend(totalMember)}<span className="add_text">회</span></span>
       </div>
       <div className='content'>
         <span className='content_title'>나이(연도)</span>
---
+++ src/lib/totalyAttend.ts
@@ -0,0 +1,17 @@
+import type { Member } from "../store/slice"
+
+export const totalyAttend = (totalMember: [string, Member][]) => {
+  const total = totalMember.reduce((acc, member) => {
+    if(member[1].attend){
+      const data = Object.values(member[1].attend).map(val => Object.values(val)).flat()
+      const result = data.reduce((memberAcc, att) => {
+        return memberAcc + att
+      }, 0)
+      const aver = result / data.length
+      return acc + aver
+    }else{
+      return acc
+    }
+  }, 0)
+  return Math.round(total / totalMember.length*100) / 100
+}
